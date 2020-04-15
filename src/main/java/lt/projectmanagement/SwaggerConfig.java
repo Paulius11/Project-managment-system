bde0858041792c9320c382a924d77b7f113dd201
Paulius <paulius.gaizauskas@gmail.com>
2020-04-15 23:44:28 +0300

    Fix swagger for tests

--- src/main/java/lt/projectmanagement/SwaggerConfig.java
+++ src/main/java/lt/projectmanagement/SwaggerConfig.java
@@ -1,7 +1,7 @@
 package lt.projectmanagement;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,25 +10,19 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
 @EnableSwagger2
-@SpringBootApplication
-public class SwagerConfig {
+public class SwaggerConfig {
+
 	@Bean
-	public Docket swaggerDocket() {
-
-		return new Docket(DocumentationType.SWAGGER_2)
-
-				.apiInfo(apiInfo()).select()
-
-				.apis(RequestHandlerSelectors.basePackage("lt.projectmanagement"))
-
-				.build();
-
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(metadata()).select()
+				.apis(RequestHandlerSelectors.basePackage("lt.projectmanagement.controller")).build();
 	}
 
-	private ApiInfo apiInfo() {
-
-		return new ApiInfoBuilder().title("Project management").version("0.0.1-SNAPSHOT").build();
+	private ApiInfo metadata() {
+		return new ApiInfoBuilder().title("Project Management System").description("CRUD api for projects and tasks")
+				.version("1.0").build();
 	}
 
 }
