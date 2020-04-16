0d35405fadb407b8d446f6b990161b87238b36c6
Paulius <paulius.gaizauskas@gmail.com>
2020-04-16 21:52:42 +0300

    Add test adding project

--- src/test/java/lt/projectmanagement/TestingWebApplicationTest.java
+++ src/test/java/lt/projectmanagement/TestingWebApplicationTest.java
@@ -10,7 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import lt.projectmanagement.doa.ProjectRepository;
+import lt.projectmanagement.doa.TaskRepository;
+import lt.projectmanagement.model.Project;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,9 +24,25 @@ public class TestingWebApplicationTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	ProjectRepository repositoryProject;
+
+	@MockBean
+	TaskRepository repositoryTask;
+
+	@MockBean
+	Project projectModel;
+
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
 		this.mockMvc.perform(get("/api/projects")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(containsString("[")));
+	}
+
+	@Test
+	public void testAddingProject() throws Exception {
+		repositoryProject.save(new Project("Exciting project-0", "Do something fun-0", true));
+		this.mockMvc.perform(get("/api/projects")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Exciting project-0")));
 	}
 }
