b2621910b8ff2c5c783fc719dd7f11474c3747cb
Paulius <paulius.gaizauskas@gmail.com>
2020-04-20 19:00:37 +0300

    Add project test

--- src/test/java/lt/projectmanagement/TestingWebApplicationTest.java
+++ src/test/java/lt/projectmanagement/TestingWebApplicationTest.java
@@ -1,10 +1,15 @@
 package lt.projectmanagement;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +49,21 @@ public class TestingWebApplicationTest {
 		repositoryProject.save(new Project("Exciting project-0", "Do something fun-0", true));
 		this.mockMvc.perform(get("/api/projects")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(containsString("Exciting project-0")));
+	}
+
+	@Test
+	@Transactional
+	public void projectSaveTest() {
+
+		final String PROJECT_NAME = "ProjektasTest";
+		List<Project> findByProjectNameList = repositoryProject.findByProjectName(PROJECT_NAME);
+		if (findByProjectNameList.size() == 0) {
+			Project project = new Project(PROJECT_NAME, "ProjectTestDescr", true);
+			repositoryProject.save(project);
+		}
+
+		findByProjectNameList = repositoryProject.findByProjectName(PROJECT_NAME);
+		assertTrue(findByProjectNameList.size() > 0);
+
 	}
 }
