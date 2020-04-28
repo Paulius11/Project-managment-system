32af451f6df9a329ab31d43ce7bf52fc60a239e8
Albert Pilecki <albert.pilecki@gmail.com>
2020-04-28 16:06:25 +0300

    post/put/delte/get

--- src/main/java/lt/projectmanagement/controller/TaskController.java
+++ src/main/java/lt/projectmanagement/controller/TaskController.java
@@ -1,15 +1,25 @@
 package lt.projectmanagement.controller;
 
+import lt.projectmanagement.model.Project;
 import lt.projectmanagement.model.Task;
+import lt.projectmanagement.model.TaskPostModel;
 import lt.projectmanagement.userservice.ProjectTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 @CrossOrigin("http://localhost:3000")
+@Api(value = "user")
 @RestController
 @RequestMapping("/api/projects/{projectId}/tasks")
 public class TaskController {
@@ -21,17 +31,47 @@ public class TaskController {
 
 	@Autowired
 	ProjectTaskService userService;
-
+	private Task createTask;
 //	Gauti visus taskus - GET /projects/{id}/tasks
+	@ApiOperation(value = "Get users", notes = "Returns registered users.")
 	@GetMapping()
 	public ResponseEntity<List<Task>> getAllTasks(@PathVariable Long projectId) {
 		return new ResponseEntity<List<Task>>(userService.getAllTasks(projectId), HttpStatus.OK);
 	}
 
 //	Gauti kontretu taską - GET /projects/{id}/tasks/{taskID}
+
 	@GetMapping("/{taskId}")
 	public ResponseEntity<Task> getSpecificTask(@PathVariable Long projectId, @PathVariable Long taskId) {
 		return new ResponseEntity<Task>(userService.getSpecificTask(projectId, taskId), HttpStatus.OK);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Object>createTask(@PathVariable Long projectId, @Valid @RequestBody TaskPostModel taskPost) {
+	Project project	= userService.geProjectById(projectId);
+	createTask = userService.createTask(taskPost, project);
+	
+
+		// Adds to response 'Header Location': http://localhost:9090/api/project/{id}/task/
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{taskID}")
+				.buildAndExpand(createTask.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	
+	@PutMapping("/{taskId}")
+	public Task editProject(@Valid @PathVariable Long projectId, @RequestBody TaskPostModel taskPost, @PathVariable Long taskId) {
+		Project projectUpdate = userService.geProjectById(projectId);
+		Task taskUpdate = userService.taskUpdate(taskId, taskPost, projectUpdate);
+		return taskUpdate;
+	}
+
+	@DeleteMapping("/{taskId}")
+	public ResponseEntity<Void> deleteTask(@PathVariable Long projectId, @PathVariable Long taskId) {
+		Project project = userService.geProjectById(projectId);
+		userService.deleteTaskById(taskId, project);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+//	    return ResponseEntity.noContent().build();
 	}
 
 }
---
+++ src/main/java/lt/projectmanagement/model/TaskPostModel.java
@@ -0,0 +1,56 @@
+package lt.projectmanagement.model;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Size;
+
+public class TaskPostModel {
+
+	public TaskState getTaskState() {
+	return taskState;
+}
+
+public void setTaskState(TaskState taskState) {
+	this.taskState = taskState;
+}
+
+	@Size(min=1)
+	private String taskName;
+	@Size(min=1)
+	private String taskDescription;
+	@Column(columnDefinition = "varchar(7) default 'NORMAL'")
+	@Enumerated(EnumType.STRING)
+	private TaskPriorityLevel taskPriority;
+
+	@Column(columnDefinition = "varchar(12) default 'TO_DO'")
+	@Enumerated(EnumType.STRING)
+	private TaskState taskState;
+	
+
+	public TaskPriorityLevel getTaskPriority() {
+		return taskPriority;
+	}
+
+	public void setTaskPriority(TaskPriorityLevel taskPriority) {
+		this.taskPriority = taskPriority;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+
+	
+	}
