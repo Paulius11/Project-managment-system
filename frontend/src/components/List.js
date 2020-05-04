b9c5ee26540736f7e03ec4d3592fdf14e528c0b6
Paulius <paulius.gaizauskas@gmail.com>
2020-05-04 16:16:26 +0300

    Add pagination

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Card, Table, ButtonGroup, Badge, Form, Button } from 'react-bootstrap';
+import { Card, Table, ButtonGroup, Badge, Form, Button, Pagination, Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faList, faEdit, faTrash, faAdjust } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../App.css';
+
 
 export default class List extends Component {
 
@@ -13,7 +14,9 @@ export default class List extends Component {
         super(props);
         this.state = {
             lists: [],
-            search: ''
+            search: '',
+            currentPage: 1,
+            postsPerPage: 3
         }
         this.api = "http://localhost:9090/api/projects";
 
@@ -62,12 +65,19 @@ export default class List extends Component {
             this.getLists();
         }
         this.setState({
+            currentPage: 1,
             search: e.target.value,
             lists: this.state.lists.filter((p) => {
                 return p.projectName.toLowerCase().includes(this.state.search.toLowerCase())
             })
         })
     };
+
+    
+    paginate = (page)  => {
+        this.setState({currentPage: page})
+        console.log(this.state.currentPage)
+    }
 
     render() {
 
@@ -81,9 +91,35 @@ export default class List extends Component {
             width: '33%'
         };
 
+
+        // Get current posts -- pagination
+        const indexOfLastPost = this.state.currentPage * this.state.postsPerPage;
+        const indexOfFirstPost = indexOfLastPost - this.state.postsPerPage;
+        const currentPosts = lists.slice(indexOfFirstPost, indexOfLastPost);
+
+
+        let items = [];
+        for (let number = 1; number <= Math.ceil(lists.length/this.state.postsPerPage); number++) {
+            items.push(
+                <Pagination.Item onClick={() => this.paginate(number)} key={number} >
+                    {number}
+                </Pagination.Item>,
+            );
+        }
+
+        const paginationBasic = (
+            <div>
+                <Pagination bsPrefix={'custom-pagination'}>{items}</Pagination>
+
+            </div>
+        );
+
+
+
         return (
 
             <Card className={"border border-dark bg-dark text-white"} >
+
                 <Card.Header><FontAwesomeIcon icon={faList} /> All projects list
                     <Form>
                         <Form.Row>
@@ -110,7 +146,7 @@ export default class List extends Component {
                         <tbody>
                             {
                                 lists.length ?
-                                    lists.map(project => {
+                                    currentPosts.map(project => {
                                         return (
                                             <tr key={project.id}>
                                                 <td>{project.id} </td>
@@ -136,11 +172,25 @@ export default class List extends Component {
                                     null
 
                             }
+                            <br />
+
+
                         </tbody>
 
                     </Table>
                 </Card.Body>
+
+                <Container>
+
+                    <Row>
+                        <Col md={{ span: 3, offset: 5 }}> {paginationBasic}</Col>
+
+                    </Row>
+
+                </Container>
             </Card >
+
+
         );
 
     }
