40630df0a477e52f14f4348d555f71740dc61dad
Paulius <paulius.gaizauskas@gmail.com>
2020-05-19 08:11:45 +0300

    Remove unused imports

--- frontend/src/components/TaskElement.js
+++ frontend/src/components/TaskElement.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { Card, ButtonGroup, Button, Badge } from "react-bootstrap";
+import { Card, Badge } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import TaskElementPopup from "./TaskElementPopup";
